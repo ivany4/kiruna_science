c846b66b0e37dc2bd60e3e90a05d57b3b66137fa
ivany4 <ivasin-1@student.ltu.se>
2012-05-08 10:28:54 +0200

    changed date of measurements

--- Optics & Radar -based observations/Assignment 2/Part2A.m
+++ Optics & Radar -based observations/Assignment 2/Part2A.m
@@ -1,12 +1,12 @@
 clear all;
 close all;
 
-data_1_len = 120;
-data_2_len = 15;
+data_1_len = 133;
+data_2_len = 32;
 
-data_1 = importdata('Height resolution/TXT_20081020_test1.FCA');
-data_2 = importdata('Height resolution/TXT_20081020_test2.FCA');
+data_1 = importdata('Height resolution/TXT_20081015_test1.FCA');
+data_2 = importdata('Height resolution/TXT_20081015_test2.FCA');
 
 plotSNR(data_1, data_1_len, '150 m Height Resolution', 2);
 plotSNR(data_2, data_2_len, '1200 m Height Resolution', 2);
-set(figure(1000), 'Name', 'SNR (2008-10-20)');
+set(figure(1000), 'Name', 'SNR (2008-10-15)');
--- Optics & Radar -based observations/Assignment 2/Part2B.m
+++ Optics & Radar -based observations/Assignment 2/Part2B.m
@@ -1,13 +1,13 @@
 clear all;
 close all;
 
-data_3_len = 110;
-data_4_len = 111;
-data_5_len = 120;
+data_3_len = 127;
+data_4_len = 126;
+data_5_len = 133;
 
-data_3 = importdata('Codes/TXT_20081020_test3.FCA');
-data_4 = importdata('Codes/TXT_20081020_test4.FCA');
-data_5 = importdata('Codes/TXT_20081020_test5.FCA');
+data_3 = importdata('Codes/TXT_20081015_test3.FCA');
+data_4 = importdata('Codes/TXT_20081015_test4.FCA');
+data_5 = importdata('Codes/TXT_20081015_test5.FCA');
 
 %% Compare coding techniques
 plotSNR(data_3, data_3_len, 'Barker coding data', 3);
@@ -20,6 +20,6 @@ plotWind(data_4, data_4_len, 'complementary coding', 3);
 plotWind(data_5, data_5_len, 'uncoded', 3);
 
 %% Figure names, focus to SNR figure
-set(figure(1002), 'Name', 'Horizontal Wind Direction [deg] (2008-10-20)');
-set(figure(1001), 'Name', 'Horizontal Wind Velocity [m/s] (2008-10-20)');
-set(figure(1000), 'Name', 'SNR (2008-10-20)');
+set(figure(1002), 'Name', 'Horizontal Wind Direction [deg] (2008-10-15)');
+set(figure(1001), 'Name', 'Horizontal Wind Velocity [m/s] (2008-10-15)');
+set(figure(1000), 'Name', 'SNR (2008-10-15)');
