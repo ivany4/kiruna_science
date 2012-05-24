1d4c6d7119e3423d327299fd4216226e5bc094c8
anuraj-rp <anuraj.rp@gmail.com>
2012-05-24 15:44:29 +0200

    git ignore

--- Space Physics/Practical_2/Part 2B/assign_PSD.m
+++ Space Physics/Practical_2/Part 2B/assign_PSD.m
@@ -38,4 +38,46 @@ b_wave = [time, b1_wave, b2_wave, b3_wave];
  xlabel('Frequency Distribution [hz]');
  ylabel('log[PSD]');
  
+ %Transforming to Magenetic Field in OB System 
+B4OB=OBsystem(b_wave ,b_FGM); 
+
+%Computing the Third Electric Field
+[e3comp]=thirdE(b_FGM,EFW_t_gse); 
+
+%Transforming to Electric Field in OB System
+E4OB=OBsystem(e3comp,b_FGM);
+
+%Seperating the componenets in new OB system
+ex=E4OB(:,2).'; % Transposing
+ey=E4OB(:,3).'; 
+ez=E4OB(:,4).'; 
+
+bx=B4OB(:,2).'; 
+by=B4OB(:,3).'; 
+bz=B4OB(:,4).'; 
+
+ %-------------------------------------------------------------------------
+ %Computing PSD of Electric Field:
+ %[vpsd,vpsddev,vpsdx,vpsdxdev,vpsdy,vpsdydev,vpsdz,vpsdzdev,vfreqs]=PSDvsFREQ(bx,by,bz,FSAMP,NK,Kstart,Kshift,N,NG,GW) 
+ 
+ [psdE,psddevE,psdEx,psdExdev,psdEy,psdEydev,psdEz,psdEzdev,Efreqs]= PSDvsFREQ(ex,ey,ez,454.5,11,1,512,2048,2048,512); 
+ 
+ figure
+ plot(Efreqs,log(psdE),'r');
+ title('Electric Field Logarithimic PSD plot');
+ xlabel('Frequency Distribution [hz]');
+ ylabel('log[PSD]');
+ %-------------------------------------------------------------------------
+ %Computing PSD of Electric Field:
+ %[vpsd,vpsddev,vpsdx,vpsdxdev,vpsdy,vpsdydev,vpsdz,vpsdzdev,vfreqs]=PSDvsFREQ(bx,by,bz,FSAMP,NK,Kstart,Kshift,N,NG,GW) 
+ 
+ [psdB,psddevB,psdBx,psdBxdev,psdBy,psdBydev,psdBz,psdBzdev,Bfreqs]= PSDvsFREQ(bx,by,bz,454.5,11,1,512,2048,2048,512); 
+ 
+ figure
+ plot(Bfreqs,log(psdB),'r');
+ title('Magnetic Field Logarithimic PSD plot');
+ xlabel('Frequency Distribution [hz]');
+ ylabel('log[PSD]');
+ 
+ 
  
